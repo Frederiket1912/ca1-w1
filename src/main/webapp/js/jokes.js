4e990159e68e414035e28e171a82ec677374ca40
Malthe Woschek <malthe.woschek@gmail.com>
2020-02-24 00:27:36 +0100

    fixed javascript for jokes

--- src/main/webapp/js/jokes.js
+++ src/main/webapp/js/jokes.js
@@ -1,5 +1,5 @@
 let url2 = "https://frederiket.dk/ca1/api/jokes/all";
-//let url2 = "http://localhost:8080/ca1/api/jokes/all";
+//let url3 = "http://localhost:8080/ca1/api/jokes/all";
 let alljokes;
 
 function loadJokesPage() {
@@ -7,13 +7,13 @@ function loadJokesPage() {
     let h1content = document.querySelector("#h1content");
     let h3content = document.querySelector("#h3content");
     let content = document.querySelector("#content");
-    let linksDiv = document.querySelector("#linksDiv");
+    let linksDivs = document.querySelector("#linksDiv");
 
     //Clear existing content
     h1content.innerHTML = "";
     h3content.innerHTML = "";
     content.innerHTML = "";
-    linksDiv.innerHTML = "";
+    linksDivs.innerHTML = "";
 
     //Show all jokes
     fetch(url2)
@@ -24,25 +24,37 @@ function loadJokesPage() {
             });
 
     //Add buttons
-    createButtons();
+    createButtons2();
 
 }
 
 function allJokes(jokes) {
-    let tableInfo = jokes.map(jokes => `<tr><td>${jokes.id}</td><td>${jokes.joke}</td></tr>`);
-    tableInfo.unshift("<table id=\"jokestable\"<table class=\"table\"><tr><th>ID</th><th>Joke</th></tr>");
+    let tableInfo = jokes.map(jokes => `<tr><td>${jokes.joke}</td></tr>`);
+    tableInfo.unshift("<table id=\"jokestable\"<table class=\"table\"><tr><th>Joke</th></tr>");
     tableInfo.push("</table>");
     return tableInfo.join('');
 }
 
-function createButtons() {
-    let linksDiv = document.querySelector("#linksDiv");
-    let b1 = "<button type=\"submit\" onclick=\"getRandomJoke()\">Random Joke</button>";
-    let filter = "<br><button type=\"submit\" onclick=\"loadSearch()\">Search by ID</button>";
-    linksDiv.innerHTML = b1 + filter;
+function createButtons2() {
+    let linksDivs = document.querySelector("#linksDiv");
+    let b1 = "<button type=\"submit\" onclick=\"randomJoke()\">Random Joke</button>";
+    let filter2 = "<br><button type=\"submit\" onclick=\"loadSearch()\">Search by ID</button>";
+    linksDivs.innerHTML = b1 + filter2;
 }
 
-function loadFilter() {
+function randomJoke(joke) {
+    let arraySize = alljokes.length;
+    let randomNum = Math.floor(Math.random() * arraySize);
+    let arrayPick = alljokes[randomNum];
+
+    console.log(arrayPick);
+    let randomTable = "<table id=\"jokestable\"<table class=\"table\"><tr><th>Joke</th></tr>" +
+            `<tr><td>${arrayPick.joke}</td></tr>`
+            + "</table>";
+    content.innerHTML = randomTable;
+}
+
+function loadSearch() {
     let linksDiv = document.querySelector("#linksDiv");
     let inputfield = "<input type=\"number\" name=\"fieldvalue\" id=\"field\" required />";
     let searchbutton = "<button type=\"submit\" onclick=\"searchFilter()\" >Search</button>";
